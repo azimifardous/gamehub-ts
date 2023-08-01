ba99366df9d7e48f9af3a596313af4c692923e35
Ahmad Fardous Azimi <ferdous.azimi7@gmail.com>
2023-08-01 13:30:11 +0300

    Created the website's layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
-  return <Button colorScheme="red">Click</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"coral"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"yellow"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"orange"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
