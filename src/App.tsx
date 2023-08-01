b5c25c0c38906153c1d01285d7332777903ecd4c
Ahmad Fardous Azimi <ferdous.azimi7@gmail.com>
2023-08-01 12:43:30 +0300

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return <Button colorScheme="red">Click</Button>;
+}
+
+export default App;
