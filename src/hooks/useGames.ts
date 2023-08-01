f26f73f8ffeb96e1acabbc587f43868e7f6620c6
Ahmad Fardous Azimi <ferdous.azimi7@gmail.com>
2023-08-01 16:24:02 +0300

    Create the loading card skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,36 +9,43 @@ export interface Platform {
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform}[];
-    metacritic: number;
-  }
-  
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-const useGames = () => {
-    const controller = new AbortController();
-
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-  
-    useEffect(() => {
-      apiClient
-        .get<FetchGamesResponse>("/games", { signal: controller.signal})
-        .then((res) => setGames(res.data.results))
-        .catch((err) => {
-            if (err instanceof CanceledError) return;
-            return setError(err.message);
-        })
-
-        return () => controller.abort();
-    }, []);
-
-    return { games, error }
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
+
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, isLoading };
+};
 
 export default useGames;
